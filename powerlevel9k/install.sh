c0774e0e6e8d7c56766b4de6015e27a432f6b967
Roberto Arce <robertoarcee@gmail.com>
2019-11-15 09:42:54 -0300

    remove powertheme9k install now use powerlevel10k direct from prezto

--- powerlevel9k/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# Powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git  ~/.zprezto/modules/prompt/external/powerlevel9k
-ln -s ~/.zprezto/modules/prompt/external/powerlevel9k/powerlevel9k.zsh-theme ~/.zprezto/modules/prompt/functions/prompt_powerlevel9k_setup

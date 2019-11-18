d0fb5021436b9e8bd2ca1e430f1a06588e802dff
Roberto Arce <robertoarcee@gmail.com>
2019-11-18 10:26:30 -0300

    add google cloud sdk

---
+++ google-cloud.sdk/completion.zsh
@@ -0,0 +1,5 @@
+# The next line enables shell command completion for gcloud.
+if [ -d "$HOME/google-cloud-sdk/completion.zsh.inc" ]
+then
+  . "$HOME/google-cloud-sdk/completion.zsh.inc"
+fi
---
+++ google-cloud.sdk/install.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl https://sdk.cloud.google.com | bash /dev/stdin --disable-prompts --install-dir="$HOME"
